01e4fba8abd99ea914e7ff79b32e987e0d003882
Daniel Gerlag <daniel@gerlag.ca>
2017-01-28 09:29:10 -0800

    initial commit

--- jworkflow.kernel/src/main/java/com/jworkflow/kernel/services/WorkflowExecutorImpl.java
+++ jworkflow.kernel/src/main/java/com/jworkflow/kernel/services/WorkflowExecutorImpl.java
@@ -2,10 +2,11 @@ package com.jworkflow.kernel.services;
 
 import com.google.inject.Inject;
 import com.jworkflow.kernel.interfaces.*;
-import com.jworkflow.kernel.models.ExecutionPointer;
-import com.jworkflow.kernel.models.WorkflowDefinition;
-import com.jworkflow.kernel.models.WorkflowInstance;
+import com.jworkflow.kernel.models.*;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
@@ -33,9 +34,90 @@ public class WorkflowExecutorImpl implements WorkflowExecutor {
             return;
         }
         
-        exePointers.forEach(pointer -> {
+        exePointers.forEach((ExecutionPointer pointer) -> {
             //pointer.setActive(true);
+            Optional<WorkflowStep> step = def.getSteps().stream().filter(x -> x.getId() == pointer.getStepId()).findFirst();
+            
+            if (step.isPresent()) {
+                
+                try {
+                    if (pointer.getStartTime() == null)
+                        pointer.setStartTime(new Date());
+                    
+                    logger.log(Level.INFO, String.format("Starting step %s on workflow %s", step.get().getName(), workflow.getId()));
+                    
+                    StepBody body = (StepBody)step.get().getBodyType().newInstance();
+                    
+                    //todo: inputs
+                    
+                    StepExecutionContext context = new StepExecutionContext();
+                    context.setWorkflow(workflow);
+                    context.setStep(step.get());
+                    context.setPersistenceData(pointer.getPersistenceData());
+                    
+                    ExecutionResult result = body.run(context);
+                    
+                    //todo: outputs
+                    
+                    if (result.isProceed()) {
+                        pointer.setActive(false);
+                        pointer.setEndTime(new Date());
+                        int forkCounter = 1;
+                        boolean noOutcome = true;
+                        
+                        StepOutcome[] outcomes = (StepOutcome[])step.get().getOutcomes().stream().filter(x -> x.getValue() == result.getOutcomeValue()).toArray();
+                        
+                        for (StepOutcome outcome : outcomes) {
+                            
+                            ExecutionPointer newPointer = new ExecutionPointer();
+                            newPointer.setActive(true);
+                            newPointer.setStepId(outcome.getNextStep());
+                            newPointer.setConcurrentFork(forkCounter * pointer.getConcurrentFork());
+                            workflow.getExecutionPointers().add(newPointer);
+                            noOutcome = false;
+                            forkCounter++;
+                        }
+                                
+                        pointer.setPathTerminator(noOutcome);
+                                                
+                        //pointer.
+                    }
+                    else {  //no proceed
+                        pointer.setPersistenceData(result.getPersistenceData());
+                        //todo: sleeps
+                    }
+                            
+                    
+                
+                } catch (InstantiationException ex) {
+                    Logger.getLogger(WorkflowExecutorImpl.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IllegalAccessException ex) {
+                    Logger.getLogger(WorkflowExecutorImpl.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                
+            }
+            else {
+                // throw
+            }
+            persistenceStore.persistWorkflow(workflow);
         });
+        
+        determineNextExecution(workflow);
+        persistenceStore.persistWorkflow(workflow);
+    }
+    
+    private void determineNextExecution(WorkflowInstance workflow) {
+        workflow.setNextExecution(null);
+        
+        workflow.getExecutionPointers().stream().filter(x -> x.isActive()).forEach(pointer -> {
+            if (pointer.getSleepUntil() != null) {
+                workflow.setNextExecution((long)0);
+                return;
+            }
+            //workflow.setNextExecution(Math.min(0, 0));
+        });
+        
     }
     
 }
