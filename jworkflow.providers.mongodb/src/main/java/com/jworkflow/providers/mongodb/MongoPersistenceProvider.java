a7138fe301644b55d2f4cdf49b80ca6aba1cd855
Daniel Gerlag <daniel@gerlag.co.za>
2017-04-08 15:10:25 -0700

    .

--- jworkflow.providers.mongodb/src/main/java/com/jworkflow/providers/mongodb/MongoPersistenceProvider.java
+++ jworkflow.providers.mongodb/src/main/java/com/jworkflow/providers/mongodb/MongoPersistenceProvider.java
@@ -2,9 +2,15 @@ package com.jworkflow.providers.mongodb;
 
 import com.jworkflow.kernel.interfaces.PersistenceProvider;
 import com.jworkflow.kernel.models.WorkflowInstance;
+import com.jworkflow.kernel.models.WorkflowStatus;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 public class MongoPersistenceProvider implements PersistenceProvider {
 
@@ -19,23 +25,31 @@ public class MongoPersistenceProvider implements PersistenceProvider {
     
     @Override
     public String createNewWorkflow(WorkflowInstance workflow) {
-        mongoOperation.save(workflow);
+        mongoOperation.insert(workflow);
         return workflow.getId();
     }
 
     @Override
     public void persistWorkflow(WorkflowInstance workflow) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //WorkflowInstance e = mongoOperation.findById(workflow.getId(), WorkflowInstance.class);
+        mongoOperation.save(workflow);
     }
 
     @Override
     public Iterable<String> getRunnableInstances() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        Query query = new Query();        
+        query.addCriteria(Criteria.where("nextExecution").lte(new Date().getTime()));
+        query.addCriteria(Criteria.where("status").is(WorkflowStatus.RUNNABLE));
+                
+        List<WorkflowInstance> result = mongoOperation.find(query, WorkflowInstance.class);
+        
+        return Arrays.asList(result.stream().map(e -> e.getId()).toArray(String[]::new));
     }
 
     @Override
     public WorkflowInstance getWorkflowInstance(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return mongoOperation.findById(id, WorkflowInstance.class);
     }
     
 }
