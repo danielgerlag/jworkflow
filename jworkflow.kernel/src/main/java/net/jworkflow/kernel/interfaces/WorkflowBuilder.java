3c0f712438c07d96aa95b6323c0f3c68ff243298
Daniel Gerlag <daniel@gerlag.ca>
2018-12-13 19:28:03 -0800

    tests

--- jworkflow.kernel/src/main/java/net/jworkflow/kernel/interfaces/WorkflowBuilder.java
+++ jworkflow.kernel/src/main/java/net/jworkflow/kernel/interfaces/WorkflowBuilder.java
@@ -3,10 +3,12 @@ package net.jworkflow.kernel.interfaces;
 import java.util.function.Consumer;
 import net.jworkflow.kernel.models.WorkflowDefinition;
 import net.jworkflow.kernel.models.WorkflowStep;
+import net.jworkflow.kernel.models.WorkflowStepInline;
 
 public interface WorkflowBuilder<TData> {
     void addStep(WorkflowStep step);
     <TStep extends StepBody> StepBuilder<TData, TStep> startsWith(Class<TStep> stepClass);
     WorkflowDefinition build(String id, int version);
-    <TStep extends StepBody> StepBuilder<TData, TStep> startsWith(Class<TStep> stepClass, Consumer<StepBuilder<TData, TStep>> stepSetup);    
+    <TStep extends StepBody> StepBuilder<TData, TStep> startsWith(Class<TStep> stepClass, Consumer<StepBuilder<TData, TStep>> stepSetup);
+    StepBuilder<TData, WorkflowStepInline.InlineBody> startsWith(StepExecutionConsumer body);
 }
