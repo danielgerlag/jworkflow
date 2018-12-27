210612c8c4e5c98ac87d37687e96d9deb86357d5
Daniel Gerlag <daniel@gerlag.ca>
2018-12-26 19:09:49 -0800

    Dynamo locks (#4)

--- samples/sample03/src/main/java/net/jworkflow/sample03/Main.java
+++ samples/sample03/src/main/java/net/jworkflow/sample03/Main.java
@@ -8,6 +8,7 @@ import java.util.Date;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import net.jworkflow.providers.aws.DynamoDBLockProvider;
 import net.jworkflow.providers.aws.SQSProvider;
 import net.jworkflow.providers.rabbitmq.RabbitMQProvider;
 import net.jworkflow.providers.redis.RedisLockServiceProvider;
@@ -30,7 +31,7 @@ public class Main {
         //module.useQueue(new RedisQueueServiceProvider(config));
         
         //module.useQueue(new SQSProvider(Region.US_WEST_1));
-        //module.useDistibutedLock(new DynamoDBLockProvider(Region.US_WEST_1, "lockTable"));
+        //module.useDistibutedLock(new DynamoDBLockProvider(Region.US_WEST_1, "jworkflowLocks"));
         
         
         module.build();
--- scratchpad/src/main/java/com/jworkflow/scratchpad/Main.java
+++ scratchpad/src/main/java/com/jworkflow/scratchpad/Main.java
@@ -20,70 +20,24 @@ import net.jworkflow.kernel.interfaces.WorkflowBuilder;
 import net.jworkflow.kernel.models.WorkflowDefinition;
 import net.jworkflow.kernel.builders.BaseWorkflowBuilder;
 import net.jworkflow.kernel.builders.DefaultWorkflowBuilder;
+import net.jworkflow.providers.aws.DynamoDBLockService;
 import net.jworkflow.sample01.HelloWorkflow;
+import software.amazon.awssdk.regions.Region;
 
 public class Main {
     public static void main(String[] args) throws Exception {        
-     
         
-        //System.out.println(Main.class.getName());
-        Class cls = Class.forName("com.jworkflow.scratchpad.Main");
+        DynamoDBLockService dlm = new DynamoDBLockService(Region.US_WEST_1, "table4");
+        System.out.println("starting...");
+        dlm.start();
+        System.out.println("started");
         
         
-        //HelloWorkflow workflow = new HelloWorkflow();
-        //BaseWorkflowBuilder baseBuilder = new BaseWorkflowBuilder();
-        //WorkflowBuilder builder = baseBuilder.UseData(workflow.getDataType());        
+        boolean lock1 = dlm.acquireLock("lock1");
+        dlm.releaseLock("lock1");
+        System.out.println(lock1);
         
-        //workflow.build(builder);
-        //WorkflowDefinition def = builder.build(workflow.getId(), workflow.getVersion());
-        
-        //public interface StepFieldConsumer<TStep extends StepBody, TData> extends BiConsumer<TStep, TData>
-        TestData data1 = new TestData(15, "hi");
-        TestData data2 = new TestData(2, null);
-        
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("nashorn");
-        
-        //new ScriptEngineManager().getEngineByName("nashorn");
-        
-        Bindings bind = engine.createBindings();
-        bind.put("data1", data1);
-        bind.put("data2", data2);
-        
-        Object result = engine.eval("data2.id = data1.id + 100", bind);
-        //TestData data3 = (TestData)engine.get("data2");
-        //engine.
-        System.out.println(result);
-        System.out.println(data2.id);
-        
-        //p.
-        
-        Object data4 = new TestData(0, null);
-        
-        data4.getClass().getField("id").set(data4, 88);
-        
-        System.out.println(((TestData)data4).id);
-        
-        //BiConsumer<Integer, TestData> cs = ((x, y) -> y.id = x);
-        
-        //Serializable r = (BiConsumer & Serializable)cs;
-        Gson gson = new Gson();        
-        String str = gson.toJson(data4);
-        System.out.println(str);
-
-        //r.
-        
-        //int a = 7;
-        //TestData b = new TestData(0, null);
-        //cs.accept(a, b);
-        
-        //System.out.println(b.getId());
-        
-        //Gson gson = new Gson();        
-        //String str = gson.toJson(def);
-        
-        //System.out.println(str);
-        
+        dlm.stop();
         
         
     }
